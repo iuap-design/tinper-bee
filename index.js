f394e0815292d80af98aebf630e47db9697875bc
Boyuzhou <386607913@qq.com>
2016-10-21 11:27:02 +0800

    fix

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 
 var Button = require('./lib/Button');
+var ButtonGroup = require('./lib/ButtonGroup');
+var ControlLabel = require('./lib/ControlLabel');
+var FormControl = require('./lib/FormControl');
+var FormGroup = require('./lib/FormGroup');
+//var Grid = require('./lib/Grid');
+var InputGroup = require('./lib/InputGroup');
+var InputGroupAddon = require('./lib/InputGroupAddon');
+var InputGroupButton = require('./lib/InputGroupButton');
+var Loadingstate = require('./lib/Loadingstate');
+
 
 
 module.exports = {
-  Button: Button
+  Button: Button,
+  ButtonGroup: ButtonGroup,
+  ControlLabel: ControlLabel,
+  FormControl: FormControl,
+  FormGroup: FormGroup,
+  InputGroup: InputGroup,
+  InputGroupAddon: InputGroupAddon,
+  InputGroupButton: InputGroupButton,
+  Loadingstate: Loadingstate
 };
