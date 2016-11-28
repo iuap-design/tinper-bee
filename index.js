ecf2c1021e87f8ff316d37af96b5d9470d7121f7
ahua52 <1468492018@qq.com>
2016-11-28 11:16:17 +0800

    去掉不必要的组件依赖

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 
 var Button = require('./lib/Button');
+var Badge = require('./lib/Badge');
+var Alert = require('./lib/Alert');
 var ButtonGroup = require('./lib/ButtonGroup');
 var ControlLabel = require('./lib/ControlLabel');
 var FormControl = require('./lib/FormControl');
 var FormGroup = require('./lib/FormGroup');
 //var Grid = require('./lib/Grid');
 var InputGroup = require('./lib/InputGroup');
-var InputGroupAddon = require('./lib/InputGroupAddon');
-var InputGroupButton = require('./lib/InputGroupButton');
 var Loadingstate = require('./lib/Loadingstate');
+var Checkbox = require('./lib/Checkbox');
+var Pagination = require('./lib/Pagination');
+var ProgressBar = require('./lib/ProgressBar');
+var RadioGroup = require('./lib/RadioGroup');
+var Switch = require('./lib/Switch');
+var Tooltip = require('./lib/Tooltip');
 
 
 
 module.exports = {
   Button: Button,
+  Badge: Badge,
+  Alert: Alert,
   ButtonGroup: ButtonGroup,
   ControlLabel: ControlLabel,
   FormControl: FormControl,
   FormGroup: FormGroup,
   InputGroup: InputGroup,
-  InputGroupAddon: InputGroupAddon,
-  InputGroupButton: InputGroupButton,
-  Loadingstate: Loadingstate
+  Loadingstate: Loadingstate,
+  Checkbox: Checkbox,
+  Pagination: Pagination,
+  ProgressBar: ProgressBar,
+  RadioGroup: RadioGroup,
+  Switch: Switch,
+  Tooltip: Tooltip
 };
