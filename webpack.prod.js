51b05f194c998807fe2d7a7d5cda29600b992f13
rong fengliang <1141591465@qq.com>
2020-09-09 12:57:27 +0800

    Update and rename webpack.pord.js to webpack.prod.js
    修改 webpack.pord.js 名称为webpack.prod.js 同时添加process.env.NODE_ENV 支持，解决直接引用构建文件因为redux构建提示的错误问题
    错误信息如下:
    ```code
    You are currently using minified code outside of NODE_ENV === 'production'. This means that you are running a slower development build of Redux. You can use loose-envify (https://github.com/zertosh/loose-envify) for browserify or DefinePlugin for webpack (http://stackoverflow.com/questions/30030031) to ensure you have the correct code for your production build.
    
    ```

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,10 @@ module.exports = merge(config, {
     output: {
         filename: 'tinper-bee.min.js',
     },
+    // 添加process.env.NODE_ENV 解决redux production 控制台提示问题
+    new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
     plugins: [
         new webpack.optimize.UglifyJsPlugin({
             output: {
